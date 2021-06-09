f377f9760de87b5c97cc7abd212a95b4abee0282
David Jeffers <68712230+DavidJeffers@users.noreply.github.com>
2021-06-09 14:27:31 -0500

    Add files via upload

---
+++ src/features/timer/Timing.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+import { RoundedButton } from '../../components/RoundedButton';
+
+export const Timing = ({ onChangeTime }) => {
+  return (
+    <>
+      
+      <View style={styles.timingButton}>
+        <RoundedButton size={75} title="15" onPress={() => onChangeTime(15)} />
+      </View>
+        <View style={styles.timingButton}>
+        <RoundedButton size={75} title="30" onPress={() => onChangeTime(30)} />
+      </View>
+      <View style={styles.timingButton}>
+        <RoundedButton size={75} title="45" onPress={() => onChangeTime(45)} />
+      </View>
+      <View style={styles.timingButton}>
+        <RoundedButton size={75} title="1hr" onPress={() => onChangeTime(59.99)} />
+      </View>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  timingButton: {
+    flex: 4.5,
+    justifyContent: "center",
+    flexDirection: "column"
+
+  }
+})
